7ad2b1a08a3b5d2a1d48b11bb1513d3220344eb9
Tigran <mtg388@mail.ru>
2016-08-27 17:18:05 +0400

    modified for  error check

--- server.js
+++ server.js
@@ -1,34 +1,23 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+const https = require('https');
+const fs = require('fs');
+const url = require('url');
 
 
-// Create a server
-http.createServer( function (request, response) {  
-   // Parse the request containing file name
+https.createServer( function (request, response) {  
    //var pathname = url.parse(request.url).pathname;
    var pathname = request.url ;
    
-   // Print the name of the file for which request is made.
    console.log("Request for " + pathname + " received.");
    
-   // Read the requested file content from file system
    fs.readFile(pathname.substr(1), function (err, data) {
       if (err) {
          console.log(err);
-         // HTTP Status: 404 : NOT FOUND
-         // Content Type: text/plain
          response.writeHead(404, {'Content-Type': 'text/html'});
       }else{    
-         //Page found     
-         // HTTP Status: 200 : OK
-         // Content Type: text/plain
          response.writeHead(200, {'Content-Type': 'text/html'});    
          
-         // Write the content of the file to response body
          response.write(data.toString());       
       }
-      // Send the response body 
       response.end();
    });   
 }).listen(8081);
